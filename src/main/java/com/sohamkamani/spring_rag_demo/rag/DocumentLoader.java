c98ba30721fcc3c9d0fee586e47d5117fa463033
AntoineMORFOUESSE <antoine.morfouesse@zenika.com>
2025-12-01 16:09:33 +0100

    liteLLM work with also embedding model, add document, update properties, use 384 dimensions, reduce temperature

--- src/main/java/com/sohamkamani/spring_rag_demo/rag/DocumentLoader.java
+++ src/main/java/com/sohamkamani/spring_rag_demo/rag/DocumentLoader.java
@@ -35,7 +35,9 @@ public class DocumentLoader implements CommandLineRunner {
                 new Document(
                         "Si des bulles apparaissent en mélangeant, c'est bon, la première phase de fermentation est terminer."),
                 new Document(
-                        "Une fois la première phase de fermentation passé, filtrer la solution dans la bouteille qui se ferme hermétiquement et patienté jusqu'a avoir du gaz qui se forme !"));
+                        "Une fois la première phase de fermentation passé, filtrer la solution dans la bouteille qui se ferme hermétiquement et patienté jusqu'a avoir du gaz qui se forme !"),
+                new Document(
+                        "La potion de soin vous apportera de bon probiotiques grâce à la levure sauvage présente sur la peau des fruits"));
        /* List<Document> documents = List.of(
                 new Document("StarlightDB is a serverless graph database designed for real-time analytics on complex, interconnected data."),
                 new Document("The core of StarlightDB is its 'Quantum-Leap' query engine, which uses speculative execution to deliver query results up to 100x faster than traditional graph databases."),
@@ -43,6 +45,7 @@ public class DocumentLoader implements CommandLineRunner {
                 new Document("StarlightDB includes a built-in visualization tool called 'Nebula' that renders interactive 3D graphs directly within the development environment for easier analysis."),
                 new Document("Security in StarlightDB is handled by 'Cosmic Shield', which provides end-to-end encryption and fine-grained access control at the node and edge level.")
         );*/
+        //TODO: faire du decoupage de chunk avec gemini
         vectorStore.add(documents);
         System.out.println("Documents loaded into VectorStore.");
     }
